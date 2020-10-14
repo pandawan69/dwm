d02e704669d67900d747fbd737c99a025daca7e1
pandawan69 <luissimas@protonmail.com>
2020-10-14 17:14:29 -0300

    Fixing termcmd command

--- dwm-git/config.h
+++ dwm-git/config.h
@@ -67,7 +67,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "$TERMINAL", NULL };
+static const char *termcmd[]  = { "st", NULL };
 
 /* print command */
 #define printcmd "scrot -s -f ~/img/Screenshots/%y.%m.%d_%H:%M:%S.png"
