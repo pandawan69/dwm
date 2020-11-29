0ba11a1c89d6e16dd8b4f57786c8af35b8141edb
pandawan69 <luissimas@protonmail.com>
2020-11-29 16:53:57 -0300

    Changing shutdown keybindings

--- config.h
+++ config.h
@@ -112,7 +112,7 @@ static Key keys[] = {
   /* Launch scripts */
 	{ MODKEY|ShiftMask,    	        XK_t,      spawn,     	   SHCMD("theme-switcher.sh") },
 	{ MODKEY|ShiftMask,    	        XK_l,      spawn,     	   SHCMD("betterlockscreen -l dim") },
-	{ MODKEY|ShiftMask|Mod1Mask,    XK_q,      spawn,     	   SHCMD("power-prompt.sh") },
+	{ MODKEY|Mod1Mask,    XK_q,      spawn,     	   SHCMD("power-prompt.sh") },
 	/* Launch programs with Super + Shift + Key */
 	{ MODKEY|ShiftMask,    	        XK_c,      spawn,     	   SHCMD("code") },
 	{ MODKEY|ShiftMask,             XK_o,      spawn,     	   SHCMD("obsidian") } ,
@@ -152,7 +152,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,		          XK_q,      quit,           {1} },
-	{ MODKEY|Mod1Mask,             	XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask|Mod1Mask,             	XK_q,      quit,           {0} },
 };
 
 /* button definitions */
